f3ec583aa1512a1e8cf7dfcae3d904a8b3f74850
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-20 19:27:14 +0300

    adding buttons and functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+
+  const addCash = (cash) => {
+    dispatch({type:'ADD_CASH', payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type:'GET_CASH', payload: cash})
+  }
+
   return (
     <div className="App">
-      
+      <div style={{fontSize:'3rem'}}>{cash}</div>
+      <div style={{display:'flex'}}>
+        <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+      </div>
     </div>
   );
 }
