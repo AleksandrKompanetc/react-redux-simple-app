76653cbedde1e3808009a81ae729e507efa7402c
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-20 21:08:38 +0300

    adding action creators

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
 
   const dispatch = useDispatch();
-  const cash = useSelector((state) => state.cash);
+  const cash = useSelector((state) => state.cash.cash);
+  const customers = useSelector((state) => state.customers.customers);
 
   const addCash = (cash) => {
     dispatch({type:'ADD_CASH', payload: cash})
@@ -14,12 +16,43 @@ function App() {
     dispatch({type:'GET_CASH', payload: cash})
   }
 
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
   return (
     <div className="App">
       <div style={{fontSize:'3rem'}}>{cash}</div>
       <div style={{display:'flex'}}>
         <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
         <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+        <button onClick={() => addCustomer(prompt())}>Add customer</button>
+      </div>
+      <div>
+        {customers.length > 0 ? 
+        <div>
+          {customers.map((customer) => 
+          <div 
+            onClick={() => removeCustomer(customer)} 
+            style={{fontSize:'2rem', border:'1px solid black', padding:'10px', marginTop:5}}
+          >
+            {customer.name}
+          </div>
+          )}
+        </div>
+        :
+        <div style={{fontSize:'2rem', marginTop:20}}>
+          NO CLIENT!
+        </div>
+      }
       </div>
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
-import { createStore } from "redux"
+import { createStore, combineReducers } from "redux"
 import { cashReducer } from "./cashReducer"
 import { customerReducer } from "./customerReducer"
-import { combineReducers } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 
-export const store = createStore(cashReducer)
+const rootReducer = combineReducers({
+  cash: cashReducer,
+  customers: customerReducer,
+})
+
+export const store = createStore(rootReducer, composeWithDevTools())
